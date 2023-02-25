a92a0105cb2d62ce775d6ef64dee5f05a5996c04
AtanasRaykovPapucharov <atanas.papucharov@gmail.com>
2023-02-25 20:48:20 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
